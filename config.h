7b1fa380ff9e8e6ec559111f4f718ef932f7b0a5
bzty64 <adithyasunil@pm.me>
2023-01-25 16:57:27 +0530

    colors

--- config.h
+++ config.h
@@ -13,9 +13,9 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_bar[]        = "#005577";
+//static const char col_bar[]        = "#005577";
 static const char col_border[]      = "#A3A3A8";
-//static const char col_bar[]   	 		= "#56b6c2";
+static const char col_bar[]   	 		= "#8abeb7";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
