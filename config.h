aecf33631613a3e986efcfc6dfd76278192ec26f
liquidsworrds <adithyasunil@pm.me>
2024-01-07 21:54:04 +0530

    fixed tags for apps + warnings

--- config.h
+++ config.h
@@ -12,10 +12,10 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font:size=12", "FiraCode Nerd Font:style=Regular:size=14" };
-static const char dmenufont[]       = "FiraCode Nerd Font:size=12";
+static const char *fonts[]          = { "FiraCode Nerd Font:style=Regular:size=12", "FontAwesome:style=Regular:size=12" };
+static const char dmenufont[]       = "FiraCode Nerd Font:style=Regular:size=12";
 
-//#include "/home/adi/.cache/wal/colors-wal-dwm.h"
+//#include "/home/$USER/.cache/wal/colors-wal-dwm.h"
 #include "colors/monochrome.h"
 
 /* tagging */
@@ -27,10 +27,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ "Gimp",      NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "LibreWolf", NULL,     NULL,           1 << 1,    0,          0,          -1,        -1 },
+	{ "Spotify",   NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",        NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
